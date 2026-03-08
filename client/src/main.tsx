0b2b57e63008fcb3366afdd93b10f892942cdfaf
Nkanyiso Ntshangase <102148695+ovonka@users.noreply.github.com>
2026-03-08 12:16:28 +0200

    main app entrance setup, i added query provider for future setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './styles/main.scss';
 import './styles/tailwind.css';
+import './styles/main.scss';
+import ThemeProvider from './app/providers/ThemeProvider';
+import QueryProvider from './app/providers/QueryProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <QueryProvider>
+          <App />
+        </QueryProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
